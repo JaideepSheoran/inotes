ba07b83e32ad947a6a5c4e282cfd6e9dddf2fcb2
JaideepSheoran <jaideepsinghsheoran@gmail.com>
2022-04-14 10:38:35 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const env = require('dotenv');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 var cookieParser = require('cookie-parser');
 env.config({path: './config.env'});
+app.use(cors({
+    origin:'http://localhost:3000', 
+    credentials:true,
+    optionSuccessStatus:200
+}));
 app.use(cookieParser());
 app.use(express.json()); // middleware
 // Creating DB Connection
